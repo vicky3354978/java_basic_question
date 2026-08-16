8f6ed863175da7ac1f5df9efb0b231b6c84a0948
vicky kumar <vk3354978@gmail.com>
2026-08-16 23:24:34 +0530

    check palindrome is completed

--- src/StringProgram/CheckStringPalindromeOrNot.java
+++ src/StringProgram/CheckStringPalindromeOrNot.java
@@ -2,15 +2,24 @@ package StringProgram;
 
 public class CheckStringPalindromeOrNot {
     static void main() {
-        String word="hhwhh";
+        String word="121";
         char charAtStart;
         char charAtLast;
         int i=0;
         int j=word.length()-1;
         while(i<j)
         {
-            if(charAtStart.charat)
+            if(word.charAt(i)==word.charAt(j))
+            {
+                i++;
+                j--;
+            }
+            else {
+                System.out.println("the given String is not palindrome ");
+                return;
+            }
         }
+        System.out.println("the given String is palindrome ");
 
     }
 }
