bbfe5fb8fabdb671e743ba60b2533cf480d4a315
vicky kumar <vk3354978@gmail.com>
2026-08-16 23:09:51 +0530

    check palindrome

---
+++ src/StringProgram/CheckStringPalindromeOrNot.java
@@ -0,0 +1,16 @@
+package StringProgram;
+
+public class CheckStringPalindromeOrNot {
+    static void main() {
+        String word="hhwhh";
+        char charAtStart;
+        char charAtLast;
+        int i=0;
+        int j=word.length()-1;
+        while(i<j)
+        {
+            if(charAtStart.charat)
+        }
+
+    }
+}
